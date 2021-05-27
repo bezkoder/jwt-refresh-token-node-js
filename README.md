dd44008cf717c2f397323027a03453fb458bbf19
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2021-05-27 11:33:36 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Node.js JWT Refresh Token with MySQL example
+JWT Refresh Token Implementation with Node.js Express and MySQL. You can know how to expire the JWT, then renew the Access Token with Refresh Token.
+
+For instruction, please visit:
+> [Node.js JWT Refresh Token example](https://bezkoder.com/jwt-refresh-token-node-js/)
+
+The code in this post bases on previous article that you need to read first:
+> [Node.js JWT Authentication & Authorization example](https://bezkoder.com/node-js-jwt-authentication-mysql/)
+
+## User Registration, User Login and Authorization process.
+
+The diagram shows flow of how we implement User Registration, User Login and Authorization process.
+
+![jwt-token-authentication-node-js-example-flow](jwt-token-authentication-node-js-example-flow.png)
+
+And this is for Refresh Token:
+
+![jwt-refresh-token-node-js-example-flow](jwt-refresh-token-node-js-example-flow.png)
+
+## More Practice:
+> [Build Node.js Rest APIs with Express, Sequelize & MySQL](https://bezkoder.com/node-js-express-sequelize-mysql/)
+
+> [Server side Pagination in Node.js with Sequelize and MySQL](https://bezkoder.com/node-js-sequelize-pagination-mysql/)
+
+> [Node.js Express File Upload Rest API example](https://bezkoder.com/node-js-express-file-upload/)
+
+> [Node.js Express File Upload with Google Cloud Storage example](https://bezkoder.com/google-cloud-storage-nodejs-upload-file/)
+
+Associations:
+> [Sequelize Associations: One-to-Many Relationship example](https://bezkoder.com/sequelize-associate-one-to-many/)
+
+> [Sequelize Associations: Many-to-Many Relationship example](https://bezkoder.com/sequelize-associate-many-to-many/)
+
+Deployment:
+> [Deploying/Hosting Node.js app on Heroku with MySQL database](https://bezkoder.com/deploy-node-js-app-heroku-cleardb-mysql/)
+
+Integration on same Server/Port:
+> [Integrate Angular 8 with Node.js Express](https://bezkoder.com/integrate-angular-8-node-js/)
+
+> [Integrate Angular 10 with Node.js Express](https://bezkoder.com/integrate-angular-10-node-js/)
+
+> [Integrate React with Node.js Express](https://bezkoder.com/integrate-react-express-same-server-port/)
+
+## Project setup
+```
+npm install
+```
+
+### Run
+```
+node server.js
+```
---
+++ app/middleware/authJwt.js
@@ -0,0 +1,95 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const db = require("../models");
+const User = db.user;
+
+const { TokenExpiredError } = jwt;
+
+const catchError = (err, res) => {
+  if (err instanceof TokenExpiredError) {
+    return res.status(401).send({ message: "Unauthorized! Access Token was expired!" });
+  }
+
+  return res.sendStatus(401).send({ message: "Unauthorized!" });
+}
+
+const verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send({ message: "No token provided!" });
+  }
+
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return catchError(err, res);
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+const isAdmin = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRoles().then(roles => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "admin") {
+          next();
+          return;
+        }
+      }
+
+      res.status(403).send({
+        message: "Require Admin Role!"
+      });
+      return;
+    });
+  });
+};
+
+const isModerator = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRoles().then(roles => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "moderator") {
+          next();
+          return;
+        }
+      }
+
+      res.status(403).send({
+        message: "Require Moderator Role!"
+      });
+    });
+  });
+};
+
+const isModeratorOrAdmin = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRoles().then(roles => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "moderator") {
+          next();
+          return;
+        }
+
+        if (roles[i].name === "admin") {
+          next();
+          return;
+        }
+      }
+
+      res.status(403).send({
+        message: "Require Moderator or Admin Role!"
+      });
+    });
+  });
+};
+
+const authJwt = {
+  verifyToken: verifyToken,
+  isAdmin: isAdmin,
+  isModerator: isModerator,
+  isModeratorOrAdmin: isModeratorOrAdmin
+};
+module.exports = authJwt;
